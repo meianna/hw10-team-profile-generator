7fc81cb4ed046994b115d67809bd14f18606c982
Meianna Oeser <meianna.oeser@gmail.com>
2020-06-19 09:23:46 -0700

    wrote html code

--- Develop/app.js
+++ Develop/app.js
@@ -144,6 +144,95 @@ function addIntern() {
 }
 display();
 
+function generateHtml() {
+  let openHtml = `<!DOCTYPE html>
+  <html lang="en">
+  
+  <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <title>My Team</title>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+      <link rel="stylesheet" href="style.css">
+      <script src="https://kit.fontawesome.com/c502137733.js"></script>
+  </head>
+  
+  <body>
+      <div class="container-fluid">
+          <div class="row">
+              <div class="col-12 jumbotron mb-3 team-heading">
+                  <h1 class="text-center">My Team</h1>
+              </div>
+          </div>
+      </div>
+      <div class="container">
+          <div class="row">
+              <div class="team-area col-12 d-flex justify-content-center">`;
+  let managerHtml = "";
+  for (let i = 0; i < managerList.length; i++) {
+    managerHtml += `<div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${managerList[i].name}</h2>
+        <h3 class="card-title"><i class="fas fa-mug-hot mr-2"></i>Manager</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID:${managerList[i].id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${managerList[i].email}">${managerList[i].email}</a></li>
+            <li class="list-group-item">Office number: ${managerList[i].officeNumber} </li>
+        </ul>
+    </div>
+</div>
+`;
+  }
+  let engineerHtml = "";
+  for (let i = 0; i < engineerList.length; i++) {
+    engineerHtml += `<div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${engineerList[i].name}</h2>
+        <h3 class="card-title"><i class="fas fa-glasses mr-2"></i>Engineer</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID:${engineerList[i].id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${engineerList[i].email}">${engineerList[i].email}</a></li>
+            <li class="list-group-item">GitHub: <a href="https://github.com/${engineerList[i].github}" target="_blank" rel="noopener noreferrer">${engineerList[i].github}</a></li>
+        </ul>
+    </div>
+</div>
+`;
+  }
+  let internHtml = "";
+  for (let i = 0; i < internList.length; i++) {
+    internHtml += `<div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${internList[i].name}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i>Intern</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID:${internList[i].id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${internList[i].email}">${internList[i].email}</a></li>
+            <li class="list-group-item">School:${internList[i].school}</li>
+        </ul>
+    </div>
+</div>
+`;
+  }
+  let closeHtml = `</div>
+  </div>
+</div>
+</body>
+
+</html>`;
+  let html = openHtml + managerHtml + engineerHtml + internHtml + closeHtml;
+  fs.writeFileSync("./index.html", html, function () {
+    console.log("file generated");
+  });
+}
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
